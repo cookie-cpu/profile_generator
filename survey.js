d1838022a768af39c47501d0cf8ddc66eda89d7c
Matthew Cook <matthewcook13987@gmail.com>
2021-02-10 00:29:56 +0000

    Added a template literal output to check that passing data into the profile object will return that data later on in the program.

--- survey.js
+++ survey.js
@@ -1,3 +1,12 @@
+
+// What's your name? => profile.name
+// What's an activity you like doing? => profile.task
+// What do you listen to while doing that? => profile.music
+// Which meal is your favourite (eg: dinner, brunch, etc.) => profile.meal
+// What's your favourite thing to eat for that meal? => profile.food
+// Which sport is your absolute favourite? => profile.sport
+// What is your superpower? In a few words, tell us what you are amazing at! => profile.skill
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -6,17 +15,24 @@ const rl = readline.createInterface({
 });
 let profile = {};
 
-rl.question("What's your name? ", (answer) => {
-  profile.name = answer;
+
+rl.question("What's your name? ", (name) => {
+  profile.name = name;
   // TODO: Log the answer in a database
-  console.log(`Thank you ${answer}`);
+  console.log(`Thank you ${name}`);
 
   rl.close();
 });
 
+
 setTimeout(() => {
   console.log(profile)
-}, 2000);
+  console.log(`${profile.name} loves listening to ${profile.music} while ${profile.task}, eating
+  ${profile.food} for ${profile.meal}, they prefer ${profile.sport} over other sports and are great at
+  ${profile.skill}`)
+}, 3000);
+
+
 
 
 
