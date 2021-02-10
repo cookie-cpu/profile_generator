46b6f91e22d8c17cd1f53510d30bd8a8d7c4197a
Matthew Cook <matthewcook13987@gmail.com>
2021-02-10 00:58:09 +0000

    Implemented code to ask several questions, store the data and console log it back. Code is functional just needs to be cleaned up.

--- survey.js
+++ survey.js
@@ -15,24 +15,62 @@ const rl = readline.createInterface({
 });
 let profile = {};
 
+// console.log(`${name} loves listening to ${music} while ${task}, eating
+//       ${food} for ${meal}, they prefer ${sport} over other sports and are great at
+//       ${skill}`);
+//       rl.close();
 
-rl.question("What's your name? ", (name) => {
-  profile.name = name;
-  // TODO: Log the answer in a database
-  console.log(`Thank you ${name}`);
+// rl.question('Whats your name? ', (name) => {
+//   rl.question('What music? ', (music) => {
+//     rl.question("what task", (task) => {
+//       rl.question("what meal", (meal) =>
+//     }
+      
+//   });
+// });
 
-  rl.close();
+/*
+console.log(`${profile.name} loves listening to ${profile.music} while ${profile.task}, eating
+      ${profile.food} for ${profile.meal}, they prefer ${profile.sport} over other sports and are great at
+      ${profile.skill}`)
+      rl.close();
+*/
+rl.question('name : ', (name) => {
+  rl.question('music : ', (music) => {
+    rl.question("task:" ,(task) => {
+      rl.question("food:", (food) => {
+        rl.question("meal:", (meal) => {
+          rl.question("sport:", (sport) => {
+            rl.question("skill", (skill) => {
+              console.log(`${name} loves listening to ${music} while ${task}, eating
+              ${food} for ${meal}, they prefer ${sport} over other sports and are great at
+              ${skill}`)
+               rl.close();
+
+            })
+          })
+        })
+      })
+    })  
+  });
 });
 
 
-setTimeout(() => {
-  console.log(profile)
-  console.log(`${profile.name} loves listening to ${profile.music} while ${profile.task}, eating
-  ${profile.food} for ${profile.meal}, they prefer ${profile.sport} over other sports and are great at
-  ${profile.skill}`)
-}, 3000);
+// setTimeout(() => {
+//   //console.log(profile)
+//   console.log(`${profile.name} loves listening to ${profile.music} while ${profile.task}, eating
+//   ${profile.food} for ${profile.meal}, they prefer ${profile.sport} over other sports and are great at
+//   ${profile.skill}`)
+// }, 4000);
 
 
+// rl.question('Please enter the first number : ', (answer1) => {
+//   rl.question('Please enter the second number : ', (answer2) => {
+//       var result = (+answer1) + (+answer2);
+//       console.log(`The sum of above two numbers is ${result}`);
+//       rl.close();
+//   });
+// });
 
 
 
