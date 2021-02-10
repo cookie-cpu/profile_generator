4541670a8512ede016ff836c517e5b2819a472eb
Matthew Cook <matthewcook13987@gmail.com>
2021-02-10 01:08:55 +0000

    Cleaned up code to remove comments and testing code and cleaned up prompts for better formatting.

--- survey.js
+++ survey.js
@@ -13,40 +13,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let profile = {};
 
-// console.log(`${name} loves listening to ${music} while ${task}, eating
-//       ${food} for ${meal}, they prefer ${sport} over other sports and are great at
-//       ${skill}`);
-//       rl.close();
 
-// rl.question('Whats your name? ', (name) => {
-//   rl.question('What music? ', (music) => {
-//     rl.question("what task", (task) => {
-//       rl.question("what meal", (meal) =>
-//     }
-      
-//   });
-// });
-
-/*
-console.log(`${profile.name} loves listening to ${profile.music} while ${profile.task}, eating
-      ${profile.food} for ${profile.meal}, they prefer ${profile.sport} over other sports and are great at
-      ${profile.skill}`)
-      rl.close();
-*/
-rl.question('name : ', (name) => {
-  rl.question('music : ', (music) => {
-    rl.question("task:" ,(task) => {
-      rl.question("food:", (food) => {
-        rl.question("meal:", (meal) => {
-          rl.question("sport:", (sport) => {
-            rl.question("skill", (skill) => {
-              console.log(`${name} loves listening to ${music} while ${task}, eating
-              ${food} for ${meal}, they prefer ${sport} over other sports and are great at
-              ${skill}`)
+rl.question("What's your name? ", (name) => {
+  rl.question("What's an activity you like doing? ", (task) => {
+    rl.question("What do you listen to while doing that? " ,(music) => {
+      rl.question("Which meal is your fave? (Breakfast, lunch or dinner) ", (meal) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (food) => {
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+            rl.question("In a few words, tell us what you are amazing at! ", (skill) => {
+              console.log(`${name} loves listening to ${music} while doing ${task}, eating ${food} for ${meal}, they prefer ${sport} over other sports and are amazing at ${skill}`)
                rl.close();
-
             })
           })
         })
@@ -55,22 +32,6 @@ rl.question('name : ', (name) => {
   });
 });
 
-
-// setTimeout(() => {
-//   //console.log(profile)
-//   console.log(`${profile.name} loves listening to ${profile.music} while ${profile.task}, eating
-//   ${profile.food} for ${profile.meal}, they prefer ${profile.sport} over other sports and are great at
-//   ${profile.skill}`)
-// }, 4000);
-
-
-// rl.question('Please enter the first number : ', (answer1) => {
-//   rl.question('Please enter the second number : ', (answer2) => {
-//       var result = (+answer1) + (+answer2);
-//       console.log(`The sum of above two numbers is ${result}`);
-//       rl.close();
-//   });
-// });
 
 
 
